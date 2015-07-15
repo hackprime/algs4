aea485c4b721cc532c1754902645d52ee26b6dec
hackprime <hackprime@gmail.com>
2015-07-15 07:40:52 +0700

    home fixes for percolation

--- part1/week_01_1/python/percolation.py
+++ part1/week_01_1/python/percolation.py
@@ -1,5 +1,7 @@
 #!/usr/local/bin/python
 # encoding: utf-8
+from .union_find import WeightedQuickFindUF
+import random
 
 import random
 class UF(object):
@@ -10,8 +12,9 @@ class Percolation(object):
 
     def __init__(self, n):
         """create N-by-N grid, with all sites blocked"""
+        self.n = n
         self.items_count = n * n
-        self.uf = UF(self.items_count)
+        self.uf = WeightedQuickFindUF(self.items_count + 2)
         self.top_index = self.items_count
         self.bottom_index = self.items_count + 1
 
@@ -25,13 +28,13 @@ class Percolation(object):
 
     def is_full(self, i, j):
         """is site (row i, column j) full?"""
-        pass
+        return self.uf.connected(self.top_idx, self.index(i, j))
 
     def percolates(self):
         """does the system percolate?"""
-        pass
+        return self.uf.connected(self.top_idx, self.bottom_idx)
 
-    def ij_ti_index(self, index):
+    def ij_ti_index(self, i, j):
         return self.n * (i - 1) + j
 
     def index_to_ij(self, index):
@@ -39,7 +42,6 @@ class Percolation(object):
 
     def visualize(self):
         pass
-
 
 
 def solution(n, t):
@@ -57,3 +59,11 @@ def solution(n, t):
 
 if __name__ == '__main__':
     print solution(20, 30).visualize()
+
+
+class IndexOutOfBoundsException(Exception):
+    pass
+
+
+class IllegalArgumentException(Exception):
+    pass
