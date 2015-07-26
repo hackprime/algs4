ede94c547f15736e6cf9abdf3fa2eb76f8c5b8b3
hackprime <hackprime@gmail.com>
2015-07-26 22:50:27 +0700

    My first java code (percolation)

--- part1/week_01_1/java/Percolation.java
+++ part1/week_01_1/java/Percolation.java
@@ -1,19 +1,100 @@
-
-
+// import java.lang.IndexOutOfBoundsException;
+// import java.lang.IllegalArgumentException;
+// 15:50
 public class Percolation {
-    public Percolation(int N) {              // create N-by-N grid, with all sites blocked
+    private int n;
+    private int top_site;
+    private int bottom_site;
+    private boolean[][] sites;
+    private WeightedQuickUnionUF uf;
+
+    private void checkArgs(int i, int j) {
+        if (i < 1 || i > n || j < 1 || j > n) {
+            throw new IndexOutOfBoundsException();
+        }
+    }
+    private void checkNodesAround(int i, int j) {
+        int item_index = ijToIndex(i, j);
+        if (i > 1 && isOpen(i - 1, j)) uf.union(item_index, ijToIndex(i - 1, j)); // bottom
+        if (i < n && isOpen(i + 1, j)) uf.union(item_index, ijToIndex(i + 1, j)); // top
+        if (j > 1 && isOpen(i, j - 1)) uf.union(item_index, ijToIndex(i, j - 1)); // left
+        if (j < n && isOpen(i, j + 1)) uf.union(item_index, ijToIndex(i, j + 1)); // right
+    }
+    private int ijToIndex(int i, int j) {
+        return n * (i - 1) + j;
+    }
+
+    public Percolation(int N) {
+        // create N-by-N grid, with all sites blocked
+        if (N <= 0) {
+            throw new IllegalArgumentException();
+        }
+
+        int uf_size = N * N + 2;
+        n = N;
+        uf = new WeightedQuickUnionUF(uf_size);
+        bottom_site = 0;
+        top_site = N * N + 1;
+        sites = new boolean[n][];
+
+        for (int i = 0; i < n; i++) {
+            sites[i] = new boolean[n];
+            for (int j = 0; j < n; j++) {
+                sites[i][j] = false;
+            }
+        }
+    }
+    public void open(int i, int j) {
+        // open site (row i, column j) if it is not open already
+        checkArgs(i, j);
+        int index = ijToIndex(i, j);
+        sites[i - 1][j - 1] = true;
+        checkNodesAround(i, j);
+        if (i == 1) uf.union(index, top_site);
+        if (i == n) uf.union(index, bottom_site);
 
     }
-    public void open(int i, int j) {          // open site (row i, column j) if it is not open already
+    public boolean isOpen(int i, int j) {
+        // is site (row i, column j) open?
+        checkArgs(i, j);
+
+        return sites[i - 1][j - 1];
     }
-    public boolean isOpen(int i, int j) {     // is site (row i, column j) open?
+    public boolean isFull(int i, int j) {
+        // is site (row i, column j) full?
+        checkArgs(i, j);
+
+        return uf.connected(top_site, ijToIndex(i, j));
     }
-    public boolean isFull(int i, int j) {     // is site (row i, column j) full?
-    }
-    public boolean percolates() {             // does the system percolate?
+    public boolean percolates() {
+        // does the system percolate?
+        return uf.connected(top_site, bottom_site);
     }
 
-    public static void main(String[] args) {   // test client (optional)
+    public static void main(String[] args) {
+        // test client (optional)
+        int i, j, k;
+        int N = Integer.parseInt(args[0]);
+        int sitesCount = N * N;
+        int[] sitesToOpen = new int[sitesCount];
 
+        for (k = 0; k < sitesCount; k++) {
+            sitesToOpen[k] = k + 1;
+        }
+        StdRandom.shuffle(sitesToOpen);
+
+        Percolation percolation = new Percolation(N);
+
+        k = 0;
+        while (k < sitesCount) {
+            i = (sitesToOpen[k] - 1) / N;
+            j = (sitesToOpen[k] - 1) % N;
+            percolation.open(i + 1, j + 1);
+            k++;
+            if (percolation.percolates()) {
+                break;
+            }
+        }
+        StdOut.println("System percolates after " + k + " sites opened");
     }
 }
---
+++ part1/week_01_1/java/PercolationStats.java
@@ -0,0 +1,83 @@
+// import .Percolation;
+public class PercolationStats {
+    private int n;
+    private int t;
+    private double[] thresholds;
+
+    public PercolationStats(int N, int T) {
+        // perform T independent experiments on an N-by-N grid
+        if (N < 1 || T < 1) {
+            throw new IllegalArgumentException();
+        }
+
+        n = N;
+        t = T;
+        thresholds = new double[T];
+        for (int i = 0; i < T; i++) {
+            thresholds[i] = getThreshold();
+        }
+    }
+    public double getThreshold() {
+        // test client (optional)
+        int i, j, k;
+        int sitesCount = n * n;
+        int[] sitesToOpen = new int[sitesCount];
+
+        for (k = 0; k < sitesCount; k++) {
+            sitesToOpen[k] = k + 1;
+        }
+        StdRandom.shuffle(sitesToOpen);
+
+        Percolation percolation = new Percolation(n);
+
+        k = 0;
+        while (k < sitesCount) {
+            i = (sitesToOpen[k] - 1) / n;
+            j = (sitesToOpen[k] - 1) % n;
+            percolation.open(i + 1, j + 1);
+            k++;
+            if (percolation.percolates()) {
+                break;
+            }
+        }
+        return (double)k / (double)sitesCount;
+    }
+    public double mean() {
+        // sample mean of percolation threshold
+        double sum = 0;
+        for (int i = 0; i < t; i++) {
+            sum = sum + thresholds[i];
+        }
+        return sum / t;
+    }
+    public double stddev() {
+        // sample standard deviation of percolation threshold
+        if (t == 1) return Double.NaN;
+
+        double sum = 0;
+        double mean = mean();
+        for (int i = 0; i < t; i++) {
+            sum = sum + Math.pow(thresholds[i] - mean, 2);
+        }
+        return sum / (t - 1);
+    }
+    public double confidenceLo() {
+        // low  endpoint of 95% confidence interval
+        return mean() - (1.96 * stddev() / Math.sqrt(t));
+    }
+    public double confidenceHi() {
+        // high endpoint of 95% confidence interval
+        return mean() + (1.96 * stddev() / Math.sqrt(t));
+    }
+
+    public static void main(String[] args) {
+        int N = Integer.parseInt(args[0]);
+        int T = Integer.parseInt(args[1]);
+
+        // test client (described below)
+        PercolationStats ps = new PercolationStats(N, T);
+        StdOut.println("mean" + "\t" + ps.mean());
+        StdOut.println("stddev" + "\t" + ps.stddev());
+        StdOut.println("95% confidence interval" + "\t" + ps.confidenceLo() + " " + ps.confidenceHi());
+    }
+}
